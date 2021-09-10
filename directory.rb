0d84df4bfb19d43a6b1e2105819881197e261ce6
Tom Baxter <thomas99baxter@gmail.com>
2021-09-10 14:46:26 +0100

    feat: user inpuy

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 # Outputs header in a multiple lines
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +14,28 @@ def print_footer(students)
   puts "Overall, we have #{students.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+
+  while !name.empty? do
+    # add student hash into an array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    # get another name from the user
+    name = gets.chomp
+  end
+
+
+  return students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
